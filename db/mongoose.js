338d83c9af9e390b99beb4109e17a0d8eee75bb3
Prince <getforprince@hotmail.com>
2019-02-27 14:20:37 +0530

    change mlab

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect(
 	// "mongodb://cluster0-shard-00-00-nzzml.mongodb.net:27017,cluster0-shard-00-01-nzzml.mongodb.net:27017,cluster0-shard-00-02-nzzml.mongodb.net:27017/test?replicaSet=Cluster0-shard-0"
-	"mongodb://admin:admin123@ds349045.mlab.com:49045/sahayata",
+	"mongodb://admin:admin123@ds351455.mlab.com:51455/sayahata",
 	{ useNewUrlParser: true }
 );
 
